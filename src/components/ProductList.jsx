57b30d29da56e7b23e0f472e2999cf68f4bbb1e9
Oleksandr-Maly <117357713+Oleksandr-Maly@users.noreply.github.com>
2023-02-08 12:26:37 +0200

    first tusk

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export const ProductList = ({ product }) => {
+  const { id, name, category, user } = product;
+
+  return (
+    <tr data-cy="Product">
+      <td className="has-text-weight-bold" data-cy="ProductId">
+        {id}
+      </td>
+
+      <td data-cy="ProductName">{name}</td>
+      <td data-cy="ProductCategory">{`${category.icon} - ${category.title}`}</td>
+
+      <td
+        data-cy="ProductUser"
+        className={classNames(
+          {
+            'has-text-link': user.sex === 'm',
+            'has-text-danger': user.sex === 'f',
+          },
+        )}
+      >
+        {user.name}
+      </td>
+    </tr>
+  );
+};
